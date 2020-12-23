4d5528d15f66fd9aae2eb1b3e15c783f21247cf7
mexalon <nabarysh@gmail.com>
2020-12-23 17:59:34 +0300

    seems to be done

--- main.py
+++ main.py
@@ -46,6 +46,18 @@ def get_shop_list_by_dishes(person_count, cook_book, *dishes):
     return shopping_list
 
 
+def read_file_and_count_lines(dir_path, file_name):
+    data_dict = {file_name: '', 'lines': 0}
+    counter = 0
+    with open(dir_path + file_name, 'r', encoding='utf-8') as f:  # у меня какие то проблемы с кодировкой по умолчанию
+        for line in f:
+            data_dict[file_name] += line
+            counter += 1
+
+    data_dict['lines'] = counter
+    return data_dict
+
+
 # Задача №1 Считывание файла
 my_file_path = 'flz/recipes.txt'
 my_cook_book = get_cook_book_from_file(my_file_path)
@@ -62,3 +74,28 @@ my_shopping_list = get_shop_list_by_dishes(my_persons_count, my_cook_book, *my_d
 print(f'\nДля приготовления {", ".join(my_dishes)} на {my_persons_count} человека необходимо купить:')
 for entry in my_shopping_list:
          print(f'{entry} : {my_shopping_list[entry]["quantity"]} {my_shopping_list[entry]["measure"]}')
+
+
+# Задача №3 сложенеие файлов
+my_dir_path = 'flz/sorted/'
+files_list = os.listdir(my_dir_path)
+if files_list:
+    all_files_list = ['Spam']
+    for each_file in files_list:
+        if each_file.split('.')[-1] == 'txt':
+            all_files_list += [read_file_and_count_lines(my_dir_path, each_file)]
+    all_files_list.pop(0)
+
+    if all_files_list:
+        sorted_all_files_list = sorted(all_files_list, key=lambda entry: entry['lines'])
+        new_file_name = 'merged_files_' + list(all_files_list[0].keys())[0].split('.')[0] +\
+                        '_to_' + list(all_files_list[-1].keys())[0].split('.')[0] + '.txt'
+        with open(new_file_name, 'w', encoding='utf-8') as w:
+            for entry in sorted_all_files_list:
+                w.write(f'{list(entry.keys())[0]}\n{entry["lines"]}\n{list(entry.values())[0]}\n')
+    else:
+        print('неверный формат файлов в папке')
+else:
+    print('похоже, папка пустая')
+
+
